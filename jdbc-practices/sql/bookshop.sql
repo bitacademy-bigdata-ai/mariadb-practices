5403d3aab3c3566fe53ce25ccb35ff21b275f757
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-11-04 11:51:29 +0900

    PreparedStatement

--- jdbc-practices/sql/bookshop.sql
+++ jdbc-practices/sql/bookshop.sql
@@ -1,3 +1,12 @@
 show tables;
 desc book;
 desc author;
+
+-- author insert
+insert into author values(null, '원수연');
+
+-- author select
+select * from author;
+
+-- author delate all;
+delete from author;
