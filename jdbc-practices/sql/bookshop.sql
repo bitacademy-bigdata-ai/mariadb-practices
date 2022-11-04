b4a10ff26ff13bba6941a0df3f798ef5db1ab3fb
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-11-04 13:43:40 +0900

    bookshop #2

--- jdbc-practices/sql/bookshop.sql
+++ jdbc-practices/sql/bookshop.sql
@@ -10,3 +10,14 @@ select * from author;
 
 -- author delate all;
 delete from author;
+
+-- book insert
+insert into book values(null, 'test', '재고있음', 3);
+
+-- book select
+select * from book;
+
+-- book delete all
+delete from book;
+
+-- findAll
